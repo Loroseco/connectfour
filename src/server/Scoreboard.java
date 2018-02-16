4debbad0b3cebd1369a04b76568db09ae1622448
Loroseco <35838540+Loroseco@users.noreply.github.com>
2018-02-16 10:49:44 +0000

    Scoreboard javadocs

--- src/server/Scoreboard.java
+++ src/server/Scoreboard.java
@@ -1,9 +1,18 @@
 package server;
 
+/**
+ * Class used to track number of games each player has won
+ * @author Loroseco
+ *
+ */
 class Scoreboard {
 	private int[] score;
 	private String[] symbol;
 	
+	/**
+	 * Constructor, allows the scoreboard to potentially support more than two players
+	 * @param symbol	Array of player symbols
+	 */
 	Scoreboard(String[] symbol) {
 		this.symbol = symbol;
 		this.score = new int[symbol.length];
@@ -12,6 +21,9 @@ class Scoreboard {
 		}
 	}
 	
+	/**
+	 * Prints all players' scores in format suitable for display
+	 */
 	void print() {
 		for (int p = 0; p < symbol.length; p++) {
 			System.out.print((p != 0) ? " , " : "");
@@ -20,6 +32,10 @@ class Scoreboard {
 		System.out.println();		
 	}
 	
+	/**
+	 * Adds one point to the chosen player
+	 * @param symbol	Chosen player symbol
+	 */
 	void add(String symbol) {
 		for (int p = 0; p < this.symbol.length; p++) {
 			if (this.symbol[p].equals(symbol)) {
