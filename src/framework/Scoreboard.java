48b347efc34b2ad131819b03968a9ded8e5a185e
Loroseco <35838540+Loroseco@users.noreply.github.com>
2018-05-24 12:37:13 +0100

    Removed ConnectScoreboard
    No need for a child class here

--- src/framework/Scoreboard.java
+++ src/framework/Scoreboard.java
@@ -1,8 +1,33 @@
 package framework;
 
-public abstract class Scoreboard
-{
-	public abstract void addScore(int winner);
+/**
+ * Class used to track number of games each player has won
+ * @author Loroseco
+ *
+ */
+public class Scoreboard {
+	private int[] score;
 	
-	public abstract void printAllScores();
+	public Scoreboard() {
+		this.score = new int[Config.NO_OF_PLAYERS];
+		for (int playerNumber = 0; playerNumber < score.length; playerNumber++) {
+			score[playerNumber] = 0;
+		}
+	}
+	
+	public void printAllScores() {
+		for (int playerNumber = 0; playerNumber < Config.NO_OF_PLAYERS; playerNumber++) {
+			System.out.println(score); //TODO: Proper output handling here
+		}	
+	}
+	
+	public void addScore(int winner) {
+		for (int playerNumber = 0; playerNumber < Config.NO_OF_PLAYERS; playerNumber++) {
+			if (playerNumber == winner) {
+				score[playerNumber]++;
+				break;
+			}
+		}
+	}
+
 }
